6e03e32298a6da70e89de541c3d4a0d3690b0e8d
Shawn <shawn.greenman@gmail.com>
2013-04-24 21:40:32 -0700

    Changed API level to 14 (4.0).  Joystick graphic is working.

--- src/com/example/RobotController/MainActivity.java
+++ src/com/example/RobotController/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.RobotController;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends Activity
@@ -12,4 +14,30 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+   @Override
+   public void onStart()
+   {
+      BluetoothAdapter btooth = BluetoothAdapter.getDefaultAdapter();
+      if (btooth != null)
+      {
+         if (btooth.isEnabled())
+         {
+            // LETS DO THIS
+            // Look for some devices
+         }
+         else
+         {
+            // Bluetooth isn't enabled...
+            Intent enableBtoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            // Eventually we will want to kindly ask the user to turn bluetooth on.
+            //startActivityForResult(enableBtoothIntent, REQUEST_ENABLE_BT);
+         }
+      }
+      else
+      {
+         // Device does not support bluetooth.
+      }
+   }
+
 }
